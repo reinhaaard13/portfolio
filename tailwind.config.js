a2af55b0c3b0a1444446888d253f323481af381e
reinhaaard13 <reinhardkevin.rk@gmail.com>
2022-04-06 21:37:06 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        dark: '#171717',
+        secondary: '#737373'
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
